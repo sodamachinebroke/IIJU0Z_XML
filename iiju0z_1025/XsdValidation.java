6018f87a00a5132caf78a762dac2ef568fc744f5
sodamachinebroke <79090166+sodamachinebroke@users.noreply.github.com>
2023-11-05 12:08:09 +0100

    Update XsdValidation.java

--- iiju0z_1025/XsdValidation.java
+++ iiju0z_1025/XsdValidation.java
@@ -17,6 +17,7 @@ public class XsdValidation {
             dBF.setNamespaceAware(true);
             DocumentBuilder dB = dBF.newDocumentBuilder();
             org.w3c.dom.Document doc = dB.parse(new File("iiju0z_kurzusfelvetel.xml"));
+            
 
             SchemaFactory schFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI); // Create a schema
                                                                                                       // factory and set
