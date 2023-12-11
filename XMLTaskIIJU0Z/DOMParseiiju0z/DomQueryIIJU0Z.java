cf888d2e4ac22e9227fb887508ea6366d3814874
sodamachinebroke <79090166+sodamachinebroke@users.noreply.github.com>
2023-12-11 14:15:35 +0100

    made DomQueryIIJU0Z, might not work, but it's there

---
+++ XMLTaskIIJU0Z/DOMParseiiju0z/DomQueryIIJU0Z.java
@@ -0,0 +1,89 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class DomQueryIIJU0Z {
+    public static void main(String[] args) {
+        File xmlFile = new File("XMLiiju0z.xml");
+        Document doc = parseXmlFile(xmlFile);
+
+        if (doc != null) {
+            doc.getDocumentElement().normalize();
+
+            // Example queries
+            querySubscribersWithMultiplePlans(doc);
+            System.out.println("-------------------------------------------------------------");
+
+            querySubscriberWithMaxBillPayment(doc);
+        } else {
+            System.out.println("The document is null");
+            System.exit(-1);
+        }
+    }
+
+    private static Document parseXmlFile(File xmlFile) {
+        Document doc = null;
+
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
+            doc = dbBuilder.parse(xmlFile);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return doc;
+    }
+
+    private static void querySubscribersWithMultiplePlans(Document doc) {
+        NodeList subscribers = doc.getDocumentElement().getElementsByTagName("subscriber");
+        for (int i = 0; i < subscribers.getLength(); i++) {
+            NodeList plans = subscribers.item(i).getParentNode().getElementsByTagName("bill");
+            if (plans.getLength() > 1) {
+                listData(subscribers.item(i).getChildNodes(), "");
+            }
+        }
+    }
+
+    private static void querySubscriberWithMaxBillPayment(Document doc) {
+        NodeList bills = doc.getDocumentElement().getElementsByTagName("bill");
+        String maxSubscriberId = "";
+        int maxPayment = Integer.MIN_VALUE;
+
+        for (int i = 0; i < bills.getLength(); i++) {
+            NodeList query = bills.item(i).getChildNodes();
+            int payment = 0;
+
+            for (int j = 0; j < query.getLength(); j++) {
+                if (query.item(j).getNodeName().equals("price_sum")) {
+                    payment += Integer.parseInt(query.item(j).getTextContent());
+                }
+            }
+
+            if (payment > maxPayment) {
+                maxPayment = payment;
+                maxSubscriberId = bills.item(i).getAttributes().getNamedItem("bill_sub").getNodeValue();
+            }
+        }
+
+        NodeList subscribers = doc.getDocumentElement().getElementsByTagName("subscriber");
+        for (int i = 0; i < subscribers.getLength(); i++) {
+            if (subscribers.item(i).getAttributes().getNamedItem("sub_id").getNodeValue().equals(maxSubscriberId)) {
+                listData(subscribers.item(i).getChildNodes(), "");
+                break; // Only print the details of the subscriber with the max payment
+            }
+        }
+    }
+
+    private static void listData(NodeList nodeList, String indent) {
+        // This method remains unchanged from the previous version
+    }
+}
+
