ee59d74878d6bdce82a3acf73fd9380b36e1018c
sodamachinebroke <erseknorbi2000@gmail.com>
2023-12-11 16:51:48 +0100

    domquery works, but very fragile oh god...

--- XMLTaskIIJU0Z/DOMParseiiju0z/DomQueryIIJU0Z.java
+++ XMLTaskIIJU0Z/DOMParseiiju0z/DomQueryIIJU0Z.java
@@ -2,6 +2,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Element;
+import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -18,11 +19,18 @@ public class DomQueryIIJU0Z {
         if (doc != null) {
             doc.getDocumentElement().normalize();
 
-            // Example queries
-            querySubscribersWithMultiplePlans(doc);
-            System.out.println("-------------------------------------------------------------");
+            querySubscribersWithMultiplePlans(doc); 
 
-            querySubscriberWithMaxBillPayment(doc);
+            querySubscriberWithMaxBillPayment(doc); 
+
+            queryInternetPlansOnOpticalCable(doc); 
+
+            querySubscriberPlansAddress(doc, "0002"); 
+
+            queryPlansWithoutTV(doc);
+
+            listPlansWithTotalPrice(doc); 
+
         } else {
             System.out.println("The document is null");
             System.exit(-1);
@@ -45,14 +53,46 @@ public class DomQueryIIJU0Z {
 
     private static void querySubscribersWithMultiplePlans(Document doc) {
         NodeList subscribers = doc.getDocumentElement().getElementsByTagName("subscriber");
+    
+        int maxPlans = 0;
+        Element maxPlansSubscriber = null;
+    
         for (int i = 0; i < subscribers.getLength(); i++) {
             Element subscriber = (Element) subscribers.item(i);
-            NodeList plans = subscriber.getElementsByTagName("bill");
-            if (plans.getLength() > 1) {
-                listData(subscriber.getChildNodes(), "");
+            String subId = subscriber.getAttribute("sub_id");
+            int planCount = countPlansForSubscriber(doc, subId);
+    
+            
+            if (planCount > maxPlans) {
+                maxPlans = planCount;
+                maxPlansSubscriber = subscriber;
             }
         }
+    
+        if (maxPlansSubscriber != null) {
+            String subId = maxPlansSubscriber.getAttribute("sub_id");
+            NodeList emails = maxPlansSubscriber.getElementsByTagName("email");
+            String firstEmail = (emails.getLength() > 0) ? emails.item(0).getTextContent() : "No Email";
+            System.out.println("Subscriber ID: " + subId + ", First Email: " + firstEmail);
+        }
     }
+    
+    private static int countPlansForSubscriber(Document doc, String subId) {
+        NodeList bills = doc.getDocumentElement().getElementsByTagName("bill");
+        int planCount = 0;
+    
+        for (int i = 0; i < bills.getLength(); i++) {
+            Element bill = (Element) bills.item(i);
+            String billSub = bill.getAttribute("bill_sub");
+    
+            if (subId.equals(billSub)) {
+                planCount++;
+            }
+        }
+    
+        return planCount;
+    }
+    
     
 
     private static void querySubscriberWithMaxBillPayment(Document doc) {
@@ -79,12 +119,101 @@ public class DomQueryIIJU0Z {
         NodeList subscribers = doc.getDocumentElement().getElementsByTagName("subscriber");
         for (int i = 0; i < subscribers.getLength(); i++) {
             if (subscribers.item(i).getAttributes().getNamedItem("sub_id").getNodeValue().equals(maxSubscriberId)) {
+                System.out.println("Highest paying customer: ");
                 listData(subscribers.item(i).getChildNodes(), "");
                 break;
             }
         }
     }
 
+
+    private static void queryInternetPlansOnOpticalCable(Document doc) {
+        NodeList internetPlans = doc.getDocumentElement().getElementsByTagName("internet");
+        System.out.println("Internet plans on Optical cable:");
+    
+        for (int i = 0; i < internetPlans.getLength(); i++) {
+            Element internetPlan = (Element) internetPlans.item(i);
+            String technology = internetPlan.getElementsByTagName("technology").item(0).getTextContent();
+            
+            if (technology.equals("optical")) {
+                String iId = internetPlan.getAttribute("i_id");
+                System.out.println(iId);
+            }
+        }
+        System.out.println("-------------------------------------------------------------");
+    }
+    private static void querySubscriberPlansAddress(Document doc, String targetSubId) {
+        NodeList bills = doc.getDocumentElement().getElementsByTagName("bill");
+    
+        for (int i = 0; i < bills.getLength(); i++) {
+            Element bill = (Element) bills.item(i);
+            if (bill.getAttribute("bill_sub").equals(targetSubId)) {
+                String planId = bill.getAttribute("bill_plan");
+                Element plan = getPlanElementById(doc, planId);
+    
+                if (plan != null) {
+                    displayAddress(plan);
+                }
+            }
+        }
+    }
+    
+    private static Element getPlanElementById(Document doc, String planId) {
+        NodeList plans = doc.getDocumentElement().getElementsByTagName("plan");
+    
+        for (int i = 0; i < plans.getLength(); i++) {
+            Element plan = (Element) plans.item(i);
+            if (plan.getAttribute("p_id").equals(planId)) {
+                return plan;
+            }
+        }
+    
+        return null;
+    }
+    
+    private static void displayAddress(Element plan) {
+        NodeList addressNodes = plan.getElementsByTagName("address");
+        if (addressNodes.getLength() > 0) {
+            Element address = (Element) addressNodes.item(0);
+            String zip = address.getElementsByTagName("ZIP").item(0).getTextContent();
+            String city = address.getElementsByTagName("city").item(0).getTextContent();
+            String street = address.getElementsByTagName("str").item(0).getTextContent();
+    
+            System.out.println("Subscriber Address: ZIP " + zip + ", City: " + city + ", Street: " + street);
+        }
+    }
+    
+    
+    private static void queryPlansWithoutTV(Document doc) {
+        NodeList services = doc.getDocumentElement().getElementsByTagName("service");
+    
+        for (int i = 0; i < services.getLength(); i++) {
+            Element service = (Element) services.item(i);
+            if (!service.hasAttribute("tv_serv")) {
+                String planId = service.getAttribute("serv_sum");
+                System.out.println("Plan without TV: " + planId);
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    private static void listPlansWithTotalPrice(Document doc) {
+        NodeList services = doc.getDocumentElement().getElementsByTagName("service");
+    
+        for (int i = 0; i < services.getLength(); i++) {
+            Element service = (Element) services.item(i);
+            String planId = service.getAttribute("serv_sum");
+            int totalPrice = Integer.parseInt(service.getElementsByTagName("price_sum").item(0).getTextContent());
+            System.out.println("Plan " + planId + " - Total Price: " + totalPrice);
+        }
+    }
+    
+    
+    
+    
     public static void listData(NodeList nodeList, String indent){
         indent += "\t";
 
